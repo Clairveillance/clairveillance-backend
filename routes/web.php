6cde168c57feb38b3ab71275774aab0ff3a9a0a1
Axel Faya <73588143+axelfaya@users.noreply.github.com>
2021-11-14 00:26:31 +0100

    fix : modify web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/{any?}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
