faa58e73444e289c16190445096530e4630e806a
Axel Faya <73588143+axelfaya@users.noreply.github.com>
2021-11-13 18:29:52 +0100

    add strict types

--- routes/channels.php
+++ routes/channels.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Broadcast;
+declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
