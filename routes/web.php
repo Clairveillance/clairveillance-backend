3fde25e6741a31aba77a556235350ce47f203fc6
Axel Faya <73588143+axelfaya@users.noreply.github.com>
2021-11-21 01:28:38 +0100

    fix: edit Redis

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Redis\Redis;
+use Redis\Redis;
 use Illuminate\Support\Facades\Route;
 
 // Redis connection and testing.
 Route::get('/redis', function () {
-    // We need to call Redis::connect() from App\Redis\Redis to be able to use the custom connection that is specified in Environment variables config file.
+    // We need to call Redis::connect() from Redis\Redis to be able to use the custom connection that is specified in Environment variables config file.
     $redis = Redis::connect();
     // After that we can use any allowed method defined in the default Redis class (Illuminate\Support\Facades\Redis).
     $visits = (int) $redis->incr('visits');
