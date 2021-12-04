1b9b98057ec0020103cf2682ca739804d44807aa
Axel Faya <73588143+axelfaya@users.noreply.github.com>
2021-12-04 22:05:35 +0100

    fix:  edit Cors middleware

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS'],
 
     'allowed_origins' => ['*'],
 
