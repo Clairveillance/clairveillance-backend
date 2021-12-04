8a902c2a480c53b3fa9c8d75023bb1668b0b8d32
Axel Faya <73588143+axelfaya@users.noreply.github.com>
2021-12-04 20:12:30 +0100

    fix: edit RedisInterface path in  Redis

--- src/Infrastructure/Database/Redis/Redis.php
+++ src/Infrastructure/Database/Redis/Redis.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Infrastructure\Database\Redis;
 
-use Database\Redis\Concerns\RedisInterface;
 use Illuminate\Redis\Connections\Connection;
 use Illuminate\Support\Facades\Redis as BaseRedis;
+use Infrastructure\Database\Redis\Concerns\RedisInterface;
 
 /**
  * * Redis.
