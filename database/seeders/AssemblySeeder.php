a21235c74118a45a9ef743d187a6e44dcd6274ad
Axel Faya <axelfaya@gmail.com>
2022-05-23 05:17:26 +0200

    fix: add LikeSeederService for better reusabilty

--- database/seeders/AssemblySeeder.php
+++ database/seeders/AssemblySeeder.php
@@ -6,15 +6,16 @@ namespace Database\Seeders;
 
 use App\Models\Assembly\AssemblyType;
 use App\Models\Assembly\AssemblyWithProfile;
-use App\Models\Image\Image;
-use App\Models\Image\ImageType;
-use App\Models\Like\Like;
-use App\Models\Like\LikeType;
 use App\Models\User\User;
+use Database\Seeders\Concerns\LikeSeederService;
 use Illuminate\Database\Seeder;
 
 final class AssemblySeeder extends Seeder
 {
+    public function __construct(public LikeSeederService $LikeSeederService)
+    {
+    }
+
     public function run(): void
     {
         AssemblyWithProfile::factory(100)->make()
@@ -31,39 +32,9 @@ final class AssemblySeeder extends Seeder
                     $assembly->assembly_type_uuid = $assembly_types->random()->uuid;
                     $users = User::where('created_at', '<=', $assembly->created_at)->get();
                     $assembly->user()->associate($users->random())->save();
-                    $likeTypeImageType = ImageType::where('name', 'likeable images')->first();
-                    if (!$likeTypeImageType) {
-                        $likeTypeImageType = new ImageType(['name' => 'likeable images']);
-                        $likeTypeImageType->save();
-                    }
-                    $likeTypeImage = Image::where('name', 'heart')->first();
-                    if (!$likeTypeImage) {
-                        $likeTypeImage = new Image([
-                            'name' => 'heart',
-                            'type' => 'jpg',
-                            'size' => '127198271',
-                            'description' => 'Just a simple heart',
-                        ]);
-                        $likeTypeImage->user()->associate($users->random())
-                            ->type()->associate($likeTypeImageType)
-                            ->save();
-                    }
-                    $likeType = LikeType::where('name', 'heart')->first();
-                    if (!$likeType) {
-                        $likeType = new LikeType(['name' => 'heart']);
-                        $likeType->image()->associate($likeTypeImage)->save();
-                    }
-                    for ($i = 0; $i < rand(1, 200); $i++) {
-                        try {
-                            $like  = new Like();
-                            $like->is_dislike = rand(1, 10) > 1 ? 0 : 1;
-                            $like->user()->associate($users->random())
-                                ->type()->associate($likeType)
-                                ->likeable()->associate($assembly->profile)
-                                ->save();
-                        } catch (\Throwable  $e) {
-                        }
-                    }
+                    $this->LikeSeederService->setUsers($users);
+                    $this->LikeSeederService->setModel($assembly);
+                    $this->LikeSeederService->save();
                     for ($i = 0; $i < rand(1, 10); $i++) {
                         try {
                             $assemblies = AssemblyWithProfile::where('uuid', '!=', $assembly->uuid)->get();
