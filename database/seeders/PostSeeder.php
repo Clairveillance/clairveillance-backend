0c0fa0cd33cd9f8414db0c95e8b198881c0f8bb1
Axel Faya <axelfaya@gmail.com>
2022-05-23 05:34:57 +0200

    feat: add LikeSeederService to PostSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\Post\Post;
-use App\Models\Post\PostType;
 use App\Models\User\User;
+use App\Models\Post\PostType;
 use Illuminate\Database\Seeder;
+use Database\Seeders\Concerns\LikeSeederService;
 
 final class PostSeeder extends Seeder
 {
+    public function __construct(public LikeSeederService $LikeSeederService)
+    {
+    }
+
     public function run(): void
     {
         Post::factory(1000)->make()
@@ -26,8 +31,10 @@ final class PostSeeder extends Seeder
                     $post_types = PostType::where('created_at', '<=', $post->created_at)->get();
                     $post->post_type_uuid = $post_types->random()->uuid;
                     $users = User::where('created_at', '<=', $post->created_at)->get();
-                    $post->user()->associate($users->random());
-                    $post->save();
+                    $post->user()->associate($users->random())->save();
+                    $this->LikeSeederService->setUsers($users);
+                    $this->LikeSeederService->setModel($post);
+                    $this->LikeSeederService->save();
                 }
             );
         dump(__METHOD__ . ' [success]');
